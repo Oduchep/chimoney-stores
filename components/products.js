1b25e6bde3dad646c136989f0dc82f6d71ad441c
Peter Okerulu <oduchep@gmail.com>
2023-01-10 16:48:35 +0100

    updates

--- components/products.js
+++ components/products.js
@@ -1,13 +1,16 @@
 import { useQuery } from '@tanstack/react-query';
 import React, { useMemo, useState } from 'react';
 import { getAllProducts } from '../features';
-import ProductCard from './product_card';
+import ProductCard from './elements/product_card';
+import ProductCardSkeleton from './skeletons/product_card_skeleton';
 
 const Products = () => {
+  const skeletonCount = [1, 2, 3, 4, 5, 6, 7, 8];
+  const [limit, setLimit] = useState(100);
   const { data: allProducts, isLoading: productsLoading } = useQuery(
-    ['products-data'],
+    ['products-data', limit],
     () => {
-      return getAllProducts();
+      return getAllProducts({ limit });
     },
   );
 
@@ -17,9 +20,11 @@ const Products = () => {
 
   return (
     <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-10 lg:gap-16">
-      {storeProducts?.map((item) => (
-        <ProductCard key={item?.id} item={item} />
-      ))}
+      {productsLoading
+        ? skeletonCount?.map((skeleton, i) => <ProductCardSkeleton key={i} />)
+        : storeProducts?.map((item) => (
+            <ProductCard key={item?.id} item={item} />
+          ))}
     </div>
   );
 };
