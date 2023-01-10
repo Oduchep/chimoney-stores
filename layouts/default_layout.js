7a271cfdcad86e6f0d0fc92bf0cd064eda417a2f
Peter Okerulu <oduchep@gmail.com>
2023-01-10 08:26:46 +0100

    layouts and layout components

---
+++ layouts/default_layout.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import Image from 'next/image';
+import { Navbar } from './components';
+
+const DefaultLayout = ({ children }) => {
+  return (
+    <div>
+      <Navbar />
+
+      <main className="px-4 md:px-8 py-8">{children}</main>
+    </div>
+  );
+};
+
+export default DefaultLayout;
